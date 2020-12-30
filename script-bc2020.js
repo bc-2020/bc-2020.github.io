674f128a9cf61f0e399b8eef862e61e13647bb25
Marko Lapic <marko.lapic@gmail.com>
2020-12-30 21:38:59 +0100

    Added display winner with gifs.

--- script-bc2020.js
+++ script-bc2020.js
@@ -6,15 +6,27 @@ var _hour = _minute * 60;
 var _day = _hour * 24;
 var timer;
 
+let result = document.getElementById('result');
+result.style.display = "none";
+
+let test = 0;
+
 function showRemaining() {
   var now = new Date();
   var distance = end.getTime() - now.getTime();
-  if (distance < 0) {
+  test++;
+  if (distance < 0 || test > 3) {
     clearInterval(timer);
-    document.getElementById('countdownTitle').innerHTML = 'THE WINNER IS MARKO';
+    //document.getElementById('countdownTitle').style.fontSize = '1.7em';
+    document.getElementById('countdownTitle').style.marginBottom = '40px';
+    document.getElementById('countdownTitle').innerHTML = 'THE WINNER IS <span class="fadein3">' + order[2].longName + '</span>';
     document.getElementById('countdown').innerHTML = '';
+
+    result.style.display = "flex";
+
     return;
   }
+  
   var days = Math.floor(distance / _day);
   var hours = Math.floor((distance % _day) / _hour);
   var minutes = Math.floor((distance % _hour) / _minute);
@@ -28,8 +40,7 @@ function showRemaining() {
 
 timer = setInterval(showRemaining, 1000);
 
-
-
+let order = null;
 
 $.ajax({
   url: 'bc2020.json',
@@ -37,10 +48,10 @@ $.ajax({
 })
   .done(function( data ) {
  
-    let order = [ 
-      { name: "L", value: data.l},
-      { name: "M", value: data.m},
-      { name: "N", value: data.n}
+    order = [ 
+      { name: "L", value: data.l, longName: 'LEO'},
+      { name: "M", value: data.m, longName: 'MARKO'},
+      { name: "N", value: data.n, longName: 'NIKOLA'}
     ];
 
     order.sort((a, b) => a.value - b.value);
@@ -48,6 +59,6 @@ $.ajax({
     $('.firstPlaceData').html(order[2].name + " " + order[2].value);
     $('.secondPlaceData').html(order[1].name + " " + order[1].value);
     $('.thirdPlaceData').html(order[0].name + " " + order[0].value);
-    $('.lastUpdateText').html('Last update: ' + data.ts);
+    //$('.lastUpdateText').html('Last update: ' + data.ts);
   });
 
